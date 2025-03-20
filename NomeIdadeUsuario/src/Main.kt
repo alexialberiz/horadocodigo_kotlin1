ee838edd6a3182e7b4f5c7aefdf36e900bceab88
Alexia Alberiz <alexiaalberiz17@gmail.com>
2025-03-20 11:01:11 -0300

    Add files via upload

---
+++ NomeIdadeUsuario/src/Main.kt
@@ -0,0 +1,16 @@
+
+//3. Escreva um programa em que o usuário informe o seu nome e em seguida o programa perguntará a idade do usuário.
+// Agora o programa deve exibir a mensagem "Olá, [NomeDoUsuario], sua idade é [idade]".
+
+fun main() {
+    var nome : String? = null
+    var idade : Int? = 0
+
+    println("Escreva o seu nome: ")
+    nome = readln()
+    println("Escreva a sua idade: ")
+    idade = readln()
+
+    println()
+
+}
---
+++ NomeUsuario/src/Main.kt
@@ -0,0 +1,11 @@
+
+//2. Escreva um programa em que o usuário informe o seu nome e exiba a mensagem "Olá, [NomeDoUsuario]".
+fun main() {
+    var nome : String? = null
+        println("Escreva seu nome: ")
+         nome = readln()
+        println("Olá, " + nome + "!")
+
+
+
+}
