77be45458623d40a0c2e841f6e989726c70716fe
Alexia Alberiz <alexiaalberiz17@gmail.com>
2025-03-20 10:57:06 -0300

    Add files via upload

---
+++ Fusca/src/Main.kt
@@ -0,0 +1,8 @@
+
+//1 Escreva um programa que crie uma variável chamada "nome_do_carro" e atribua-a um valor "Fusca". Exiba-a ao usuário.
+fun main() {
+    val nome_do_carro = "Fusca!"
+
+        println(nome_do_carro)
+
+}
